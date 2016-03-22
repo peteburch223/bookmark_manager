5611830244a2ca7de63497f0146e74e4b421176b
Pete Burch <peteburch223@gmail>
2016-03-22 15:08:04 +0000

    #13 after walkthough check

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,5 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require 'dotenv'
-
 
 class Link
   include DataMapper::Resource
@@ -12,25 +10,8 @@ class Link
   property :date,   Date
 end
 
-Dotenv.load
+puts "Database: bookmark_manager_#{ENV['RACK_ENV']}"
 
-if ENV['RACK_ENV'] == 'development'
-  ENV['DATABASE'] = ENV['DATABASE_DEVT']
-else
-  ENV['DATABASE'] = ENV['DATABASE_TEST']
-end
-
-USER = ENV['USER']
-PASSWORD = ENV['PASSWORD']
-HOSTNAME = ENV['HOSTNAME']
-DATABASE = ENV['DATABASE']
-
-
-puts ENV['RACK_ENV']
-puts ENV['DATABASE_DEVT']
-
-puts "postgres://#{USER}:#{PASSWORD}@#{HOSTNAME}/#{DATABASE}"
-
-DataMapper.setup(:default,"postgres://#{USER}:#{PASSWORD}@#{HOSTNAME}/#{DATABASE}")
+DataMapper.setup(:default,"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize.auto_upgrade!
